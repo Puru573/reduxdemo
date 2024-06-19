69ea415afe591584d9c32402aeb8e0d1c246a613
Puru573 <purukumar.p@gmail.com>
2024-06-19 13:03:23 +0530

    bat.js changes

--- src/Component/Bat.js
+++ src/Component/Bat.js
@@ -1,30 +1,32 @@
 import React from 'react'
-import { connect } from 'react-redux';
+import {useSelector,useDispatch } from 'react-redux';
 import { buyBat } from '../redux/actionCreator';
 
 function Bat(props) {
+  const Bat=useSelector((state)=>state.bat.bat)
+  const dispatch=useDispatch();
   return (
     <div>
-        <h1>Bat : {props.bat}</h1>
-      <button onClick={()=>props.buyBat()}>button</button>
+        <h1>Bat : {Bat}</h1>
+      <button onClick={()=>dispatch(buyBat())}>button</button>
     </div>
   )
 }
 
-const mapStateToProps=(state)=>{
-    return{
-        bat:state.bat.bat
-    }
-}
+// const mapStateToProps=(state)=>{
+//     return{
+//         bat:state.bat.bat
+//     }
+// }
 
-const mapDispatchToProps=(dispatch)=>{
-    return{
-        buyBat:()=>dispatch(buyBat())
-    }
-}
+// const mapDispatchToProps=(dispatch)=>{
+//     return{
+//         buyBat:()=>dispatch(buyBat())
+//     }
+// }
 
 // mapStateToProps work is to accesss the atate value which is avaliable from the global store. to the bat component
 // mapDispatchToprops work is to send the action from component to reducer to change the state value in the reducer.
  
 
-export default connect(mapStateToProps,mapDispatchToProps)(Bat)
+export default Bat;
